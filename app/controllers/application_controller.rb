0593fa1cc047727f82a082c8428206c05239c628
rachelkathleen <rachel.hawa@gmail.com>
2019-06-29 23:33:08 -0500

    fix js and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
   def home
-    redirect_to about_path
+
   end
 
-  def about
-  end
-
-  def contact
-  end
-
-  def resume
-  end
-
-  def projects
-  end
+  # def about
+  # end
+  #
+  # def contact
+  # end
+  #
+  # def resume
+  # end
+  #
+  # def projects
+  # end
 end
